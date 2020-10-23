53ea84fdbed92319a0be4611355f33bb5ff1ca90
= <=>
2020-10-22 23:20:29 -0300

    Haciendo pruebas de PID utilizando el ejemplo del profe

--- src/hola.c
+++ src/hola.c
@@ -3,8 +3,31 @@
 #include<signal.h>
 #include<stdlib.h>
 
+void signal_handler( int signal_num ) { 
+	static int trials = 0;
+	
+	trials++;
+
+	printf("Interrupt signal is (%d).\n", signal_num );
+	//std::cout << "Interrupt signal is (" << signal_num << ").\n"; 
+  
+	if(trials == 2){
+		exit(signal_num);  
+	} 
+} 
+
 int main(int argc, char const *argv[])
 {
-	printf("Hola mundo\n");
-	return 0;
+	int count=0;
+	signal(SIGTERM, signal_handler);   
+	signal(SIGINT, signal_handler);  
+
+	while(++count) { 
+		sleep(1);
+		printf("Hello... PID=%d\n",getpid());
+		//std::cout << "Hello ... PID=" << getpid() << std::endl; 
+		
+	}
+	
+	return EXIT_SUCCESS;
 }
